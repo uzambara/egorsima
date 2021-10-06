8f1d402297705e49dbcd2aff719e278049b875fd
egor.simonov <egor.simonov@ship-cheaper.com>
2021-10-06 11:56:53 +0400

    Added images

---
+++ src/Assets/index.ts
@@ -0,0 +1,13 @@
+import close from './Icons/modalClose.svg';
+import call from './Icons/call.png';
+import email from './Icons/email.png';
+import telegram from './Icons/telegram.png';
+import whatsapp from './Icons/whatsapp.png';
+
+export const Icons = {
+  close,
+  call,
+  email,
+  telegram,
+  whatsapp,
+};
